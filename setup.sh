f70ebc850014242afc8e2909c7afd5871732642c
Nino Kiers <nino.kiers@gmail.com>
2025-05-08 11:40:47 +0200

    Added cloud variables to config

--- setup.sh
+++ setup.sh
@@ -11,7 +11,16 @@ read -rp "Enter remote username (on Pi 5): " SSH_REMOTE_USER
 read -rp "Enter remote LAN IP address (of Pi 5): " SSH_REMOTE_HOST
 read -rp "Enter the Cloudflare API token: " CF_API_TOKEN
 read -rp "Enter the Cloudflare root domain (zone name, e.g. hanobayreef.com): " CF_ZONE_NAME
+read -rp "Cloudflare R2 Access Key ID: " AWS_ACCESS_KEY
+read -rsp "Cloudflare R2 Secret Access Key: " AWS_SECRET_KEY
+echo
+read -rp "Cloudflare R2 Endpoint URL: " AWS_ENDPOINT
+read -rp "Cloudflare R2 Bucket Name: " AWS_BUCKET
 
+echo "AWS_ACCESS_KEY=$AWS_ACCESS_KEY" >> "$CONFIG_FILE"
+echo "AWS_SECRET_KEY=$AWS_SECRET_KEY" >> "$CONFIG_FILE"
+echo "AWS_ENDPOINT=$AWS_ENDPOINT" >> "$CONFIG_FILE"
+echo "AWS_BUCKET=$AWS_BUCKET" >> "$CONFIG_FILE"
 echo "SSH_REMOTE_USER=$SSH_REMOTE_USER" >> "$CONFIG_FILE"
 echo "SSH_REMOTE_HOST=$SSH_REMOTE_HOST" >> "$CONFIG_FILE"
 echo "CF_API_TOKEN=$CF_API_TOKEN" >> "$CONFIG_FILE"
@@ -19,7 +28,7 @@ echo "CF_ZONE_NAME=$CF_ZONE_NAME" >> "$CONFIG_FILE"
 
 # Install dependencies
 sudo apt update
-sudo apt install -y ffmpeg sqlite3 rsync curl openssh-client jq
+sudo apt install -y ffmpeg sqlite3 rsync curl openssh-client jq awscli
 
 # Set up SSH authorization
 echo "Generating SSH key..."
